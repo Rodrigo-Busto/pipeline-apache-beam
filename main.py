fccfb30915d4964c960f5ea3b8f1afacd3feca3a
Rodrigo Busto <rodrigo.busto@cerc.inf.br>
2021-08-25 19:41:14 -0300

    aula 2

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-import apache_beam
-print("Apache beam importado")
-print(dir(apache_beam))
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.options.pipeline_options import PipelineOptions
+
+pipeline_options = PipelineOptions(argv = None)
+pipeline = beam.Pipeline(options = pipeline_options)
+
+dengue = (
+    pipeline
+    | "Leitura do dataset de dengue" >> 
+        ReadFromText("./alura-apachebeam-basedados/casos_dengue.txt", skip_header_lines=1)
+)
